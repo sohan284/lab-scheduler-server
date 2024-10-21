43f0c741de90b016cc046f94a5d2cf21e4b2c2b7
sohanur <sr.sohan088@gmail.com>
2024-10-21 15:59:39 +0600

    add data

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,53 +1,45 @@
 const { getDB } = require("../config/db");
 
-const getTasks = async (req, res) => {
-  try {
-    const db = getDB();
-    const tasks = await db.collection("tasks").find({}).toArray();
-
-    res.status(200).json(tasks);
-  } catch (error) {
-    console.error("Error retrieving tasks:", error);
-    res
-      .status(500)
-      .json({ message: "Internal Server Error", error: error.message });
-  }
-};
 const createTask = async (req, res) => {
   try {
-    const {
-      taskName,
-      course,
-      startDate,
-      selectedTimeSlots,
-      selectedMachine,
-      email,
-    } = req.body;
-
-    if (!Array.isArray(selectedTimeSlots) || selectedTimeSlots.length === 0) {
-      return res
-        .status(400)
-        .json({ message: "Selected time slots are required." });
-    }
-    const db = getDB();
-    await db.collection("tasks").insertOne({
-      taskName,
-      course,
-      startDate,
-      selectedTimeSlots,
-      selectedMachine,
-      email,
+    const taskData = req.body;
+    const tasksCollection = getDB("lab-scheduler").collection("tasks");
+    const result = await tasksCollection.insertOne(taskData);
+    res.status(201).json({
+      success: true,
+      data: { _id: result.insertedId, ...taskData },
+      message: "Task created successfully",
     });
-
-    res.status(201).json({ message: "Task scheduled successfully!" });
   } catch (error) {
-    console.error("Error scheduling task:", error);
-    res
-      .status(500)
-      .json({ message: "Internal Server Error", error: error.message });
+    console.error("Error creating task:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to create task",
+      error: error.message,
+    });
   }
 };
 
+const getTasks = async (req, res) => {
+  try {
+    const tasksCollection = getDB("lab-scheduler").collection("tasks");
+
+    const result = await tasksCollection.find().toArray();
+
+    res?.status(200).json({
+      success: true,
+      data: result,
+      message: "Task retrieved successfully",
+    });
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    res?.status(500).json({
+      success: false,
+      error: "Failed to fetch tasks",
+      message: error.message,
+    });
+  }
+};
 module.exports = {
   getTasks,
   createTask,
