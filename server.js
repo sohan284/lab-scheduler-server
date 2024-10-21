f52146181b2343610cb1efcc9be6921723511709
sohanur <sr.sohan088@gmail.com>
2024-10-21 10:35:48 +0600

    add site

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-app.use(cors());
+// CORS configuration
+const allowedOrigins = ["http://localhost:5173"]; // Replace with your actual frontend URL
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow credentials (cookies, authorization headers)
+  })
+);
+
 app.use(express.json());
 
 connectDB();
@@ -16,11 +26,12 @@ app.get("/", (req, res) => {
   res.send("Hello From Scheduler!");
 });
 
-module.exports = app;
-
+// Start the server only if this file is run directly
 if (require.main === module) {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 }
+
+module.exports = app;
